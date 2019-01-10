e21ed5d0f2d0fc48af0dd6e3e4dfb2628ed743d6
Tommy Cervenka <drumsmylife@Tommys-MacBook-Pro.local>
2019-01-09 21:25:06 -0600

    fixed html and cleaned up JS and added start button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,55 +1,24 @@
 // create question variables
-var questions = 
-{
-    q1:  ['What band plays all the small things ?'],
-    q2:  ['Who is the captain of the Chicago Blackhawks ?'],
-    q3:  ['Who scored the stanley cup winning goal for the Blackhawks in 2010 ?'],
-    q4:  ['Who is the drummer for Blink-182 ?'],
-    q5:  ['What movie is this quote from ? "Tonight we are the greatest hockey team in the world'],
-    q6:  ['Name this song by the lyrics. "when I was a young boy my father took me into the city"'],
-};
+var question = [
+        'What band plays all the small things ?',
+        'Who is the captain of the Chicago Blackhawks ?',
+        'Who scored the stanley cup winning goal for the Blackhawks in 2010 ?',
+        'Who is the drummer for Blink-182 ?',
+        'What movie is this quote from ? "Tonight we are the greatest hockey team in the world',
+        'Name this song by the lyrics. "when I was a young boy my father took me into the city"'
+        ];
 
-var answers =
-{
-    answers1: [{a: 'sum 41',
-                b: 'green day',
-                c: 'all american rejects',
-                d: 'blink-182' //correct
-                }],
-    
-    answers2: [{a: 'jonathon quick',
-                b: 'jonathon tavares',
-                c: 'jonathon toews', //correct
-                d: 'johnny bravo'
-                }],
+var answer =
+[
+    ['sum 41','green day','all american rejects','blink-182'],          
+    ['jonathon quick','jonathon tavares','jonathon toews','johnny bravo'],
+    ['patrick kane','brent seabrook','duncan keith','dustin byfuglien'],
+    ['dave grohl','neil peart','travis barker','tre cool'],
+    ['slap shot','miracle', 'mighty ducks','goon'],
+    ['helena', 'under and over it', 'teenagers', 'welcome to the black parade']
+];
+               
 
-    answers3: [{a: 'patrick kane',//correct
-                b: 'brent seabrook',
-                c: 'duncan keith',
-                d: 'dustin byfuglien'
-                 }],
-
-    answers4: [{
-                a: 'dave grohl',
-                b: 'neil peart',
-                c: 'travis barker', //correct
-                d: 'tre cool'
-                }],
-
-    answers5: [{
-                a: 'slap shot',
-                b: 'miracle',
-                c: 'mighty ducks',
-                d: 'goon'
-                }],
-
-    answers6: [{
-                a: 'helena',
-                b: 'under and over it',
-                c: 'teenagers',
-                d: 'welcome to the black parade' //correct
-                }]
-};
 
 //counter variables
 var isCorrect = false;
@@ -61,26 +30,34 @@ var numberWrong = 0;
 //used to reset time
 var interval;
 
-//start button
-var button= document.createElement(button);
-button.innerHTML = "startBtn";
 
 //functions
-function gameStart()
-{
-    $('.startBtn').on('click',function()
-    {
+function gameStart() {
     //hide start button
-    $('.startBtn').hide();
-    
-    timeCount();
-    question1()
-    chooseCheckAnswer();
-    });
+    $('#startButton').hide();
+    var q= question[0];
+    $('#questions').html(q);
+    var a= answer [0];
+    var p=""
+    for (var i = 0;i<a.length;i++){
+        switch(i){
+            case 0:
+                l="a: ";
+                break;
+            case 1: 
+                l="b: ";
+                break;
+            case 2:
+                l="c: ";
+                break;
+            case 3:
+                l="d: ";
+                break;
 
-}
-function timeCount()
-{
-
+        }
+        
+         p+= "<div>"+ l +a[i]+ "</div>";
+    }
+    $('#answers').html(p);
 
 }
